3d9e9ff89b0027429c1bfb98ac214d1ff5b97269
selftech <selftechacademy@gmail.com>
2024-02-15 23:37:13 -0600

    added language option

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { useTranslation, Trans } from "react-i18next";
+import LanguageSwitcher from "./components/languageSwitcher/LanguageSwitcher";
+
 import "./App.css";
 
+// const lngs = {
+// 	en: { nativeName: "English" },
+// 	ru: { nativeName: "Russian" },
+// 	tr: { nativeName: "Turkish" },
+// };
+
 function App() {
-	return <div className="App">App JS</div>;
+	const { t } = useTranslation();
+
+	return (
+		<div className="App">
+			<LanguageSwitcher />
+			<header className="App-header">
+				<p>
+					<Trans i18nKey="description.part1">Hello sir</Trans>
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					{t("description.part2")}
+				</a>
+			</header>
+		</div>
+	);
 }
 
-export default App;
+// here app catches the suspense from page in case translations are not yet loaded
+export default function WrappedApp() {
+	return (
+		<Suspense fallback="...is loading">
+			<App />
+		</Suspense>
+	);
+}
---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+	reducer: {
+		// language: languageSlice,
+	},
+});
+
+export default store;
