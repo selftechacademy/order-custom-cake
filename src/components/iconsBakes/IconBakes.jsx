3f9a306e8c1d9cfd92677dc50f154082531c08cd
meerim <mmusaeva2023@gmail.com>
2024-02-26 19:31:26 -0600

    feat: icon bakes added

--- src/components/iconsBakes/IconBakes.jsx
+++ src/components/iconsBakes/IconBakes.jsx
@@ -1,13 +1,6 @@
-/* eslint-disable*/
+/*eslint-disable*/
 import React from "react";
-import classNames from "classnames";
 import { useTranslation } from "react-i18next";
-import croissant from "../../components/iconsBakes/assets/croissant.png";
-import cake from "../../components/iconsBakes/assets/cake.png";
-import fresh from "../../components/iconsBakes/assets/fresh.png";
-import time from "../../components/iconsBakes/assets/time.png";
-import check from "../../components/iconsBakes/assets/check.png";
-import chiefIcon from "../../components/iconsBakes/assets/chief.png";
 import quality from "../../components/iconsBakes/assets/quality icon.jpeg";
 import cake2 from "../../components/iconsBakes/assets/customcake.jpeg";
 import "./iconBakes.style.scss";
@@ -45,67 +38,19 @@ const icons = [
 	},
 ];
 
-const IconsBakes = ({ variant, backgroundColor, boxShadow }) => {
+const IconsBakes = () => {
 	const { t } = useTranslation();
 	return (
-		<div
-			className={classNames(`icons-container ${variant}`, {
-				"icons-container--style2": variant === "style2",
-				"icons-container--style3": variant === "style3",
-			})}
-		>
+		<div className="icons-container">
 			<div className="topic-container">
-				<h2
-					className={classNames("topic-text", {
-						"topic-text--style2": variant === "style2",
-						"topic-text--style3": variant === "style3",
-					})}
-				>
-					Why is ChicagoBakes the right choice?
-				</h2>
+				<h2 className="topic-text">Why is ChicagoBakes the right choice?</h2>
 			</div>
-			<div
-				className={classNames("icons-box", {
-					"icons-box--style2": variant === "style2",
-					"icons-box--style3": variant === "style3",
-				})}
-			>
+			<div className="icons-box">
 				{icons.map((el, index) => (
-					<div
-						key={index}
-						className={classNames("icons-item", {
-							"icons-item--style2": variant === "style2",
-							"icons-item--style3": variant === "style3",
-						})}
-						style={{
-							backgroundColor: backgroundColor,
-							boxShadow: boxShadow,
-						}}
-					>
-						<img
-							className={classNames("icon", {
-								"icon--style2": variant === "style2",
-								"icon--style3": variant === "style3",
-							})}
-							src={el.icon}
-							alt="icon"
-						/>
-						<h4
-							className={classNames("item-name", {
-								"item-name--style2": variant === "style2",
-								"item-name--style3": variant === "style3",
-							})}
-						>
-							{t(el.text)}
-						</h4>
-						<p
-							className={classNames("description", {
-								"description--style2": variant === "style2",
-								"description--style3": variant === "style3",
-							})}
-						>
-							{t(el.description)}
-						</p>
+					<div key={index} className="icons-item">
+						<img className="icon" src={el.icon} alt="icon" />
+						<h4 className="item-name">{t(el.text)}</h4>
+						<p className="description">{t(el.description)}</p>
 					</div>
 				))}
 			</div>
