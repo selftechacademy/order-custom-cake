73c2cb3a7077d5b64245999d0e389b2d0208c4b4
selftech <selftechacademy@gmail.com>
2024-03-01 20:10:03 -0600

    fix: renamed the logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 /*eslint-disable*/
 // export without file type. Dont type jsx at then end.
 export { default as OrderPage } from "./orderPage/OrderPage";
-export { default as LoginPage } from "./loginPage/LoginPage";
+export { default as LoginPage } from "./LoginPage/LoginPage";
 export { default as HomePage } from "./homePage/HomePage";
 export { default as DashboardPage } from "./dashboardPage/DashboardPage";
 export { default as SignupPage } from "./signupPage/SignupPage";
