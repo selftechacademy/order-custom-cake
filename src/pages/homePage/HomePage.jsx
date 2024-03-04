548e5e2fbb89e97e7a0acb54027866cffb7de686
Azim <azimarynaliev@icloud.com>
2024-03-03 23:14:07 -0600

    chore: commented profile tab in the home page

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -16,7 +16,7 @@ export default function HomePage() {
 	return (
 		<div>
 			<Navbar />
-			<ProfileTab />
+			{/* <ProfileTab /> */}
 			<HeroSection imgSrc={imgSrc} onClickHandler={onClickHandler} />
 			<IconsBakes />
 			<Testimonials />
